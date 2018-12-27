7d947daa015cbf3199a59c647ea8d00efb49be6d
gloxon <gloxongp@gmail.com>
2018-12-27 12:00:14 +0100

    fix issue with first time migration

--- main/defaults.py
+++ main/defaults.py
@@ -1,14 +1,22 @@
+from django.contrib.auth.models import UserManager
 from django.contrib.auth import get_user_model as UserModel
+from django.db.utils import ProgrammingError
+
+user_manager = UserManager()
 
 
 def get_default_username():
     # Generates a random unique username for the user.
+
     while True:
-        username = UserModel().objects.make_random_password(
+        username = user_manager.make_random_password(
             length=10, allowed_chars='abcdefghjkmnpqrstuvwxyz'
                                      '23456789'
         )
-        if not UserModel().objects.filter(username=username).exists():
+        try:
+            if not UserModel().objects.filter(username=username).exists():
+                return username
+        except Exception:
             return username
 
 
